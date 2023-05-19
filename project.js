b8e7b31dcde1d24910c998f3ab430fdb64282e1d
Nada-AbuHantash <s11821291@stu.najah.edu>
2023-05-20 02:29:40 +0300

    nada1

--- project.js
+++ project.js
@@ -5,17 +5,24 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 const { log, count } = require("console");
+const { request } = require("http");
+const bcrypt = require('bcrypt');
+// const { https } = require('https');
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-var TABLE = 'customer';
+
 const pool = mysql.createConnection({
 
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'advance',
-    dateStrings: true
-
+    dateStrings: true,
+    // insecureAuth: false, 
+    // ssl: {
+    //   rejectUnauthorized: true, // Ignore certificate validation (for testing only)
+    //   // other SSL/TLS options if needed
+    // },
 });
 
 console.log("hii");
@@ -23,7 +30,47 @@ pool.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL database');
   });
+  
 //////////
+app.post('/www.linkedin.com/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ error: 'Name, email, and password are required' });
+    }
+  
+    try {
+       
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        let query1=`SELECT * FROM login where email='${email}' and name='${name}'`;
+        pool.query(query1, [name,email], function(err,results) {
+          if (err) {
+            console.error('Error storing user in database:', err);
+            return res.status(500).json({ error: 'An error occurred while creating the user' });
+          }
+          else if(results.length ===0){
+        let query="INSERT INTO login (name,email,password)VALUES(?,?,?) ";
+        pool.query(query, [name,email,hashedPassword], function(err) {
+          if (err) {
+            console.error('Error storing user in database:', err);
+            return res.status(500).json({ error: 'An error occurred while creating the user' });
+          }
+          console.log( "User created successfully");
+          return res.status(201).json({ message: 'User created successfully' });
+        });
+      }
+      else {
+        console.log( "The User already exists");
+        return res.status(201).json({ message: 'The user already exists' });
+      }
+      });
+      } catch (error) {
+        console.error('Error creating user:', error);
+        return res.status(500).json({ error: 'An error occurred while creating the user' });
+      }
+    });
+ 
 
 //////////
   app.listen(3000, function () {
