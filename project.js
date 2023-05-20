bcf8273b2942e082aafaf16cc86201e45650958e
rah <rahafmelhem2001@icloud.com>
2023-05-20 23:28:44 +0300

    rahaaff

--- project.js
+++ project.js
@@ -11,21 +11,14 @@ const bcrypt = require('bcrypt');
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-const users = require('./models/user')
-const jobs = require('./models/job')
-const cvs = require('./models/cv')
+
 
 const app = express()
 const PORT = process.env.PORT
 const router = express.Router()
 
 
-app.use(cors())
-app.use(express.json())
 
-app.use(users)
-app.use(jobs)
-app.use(cvs)
 
 const pool = mysql.createConnection({
 
@@ -95,105 +88,49 @@ app.post('/www.linkedin.com/register', async (req, res) => {
 
 
 //add new job
-router.post("/newjob",async (req,res)=>{
-  const job = new jobs (req.body)
+//idjob	idcustmer	cv	coverletter	id	
+
+app.post('/www.linkedin.com/addcv', async (req, res) => {
+  const { idjob, idcustmer, cv,coverletterm,id } = req.body;
+  if (!idjob || !idcustmer || !cv  || !coverletterm || !id) {
+    return res.status(400).json({ error: 'are required' });
+  }
+
   try {
-      await job.save()
-      res.status(201).send({job})
+     
+      const saltRounds = 10;
+      const salt = await bcrypt.genSalt(saltRounds);
+      const hashedPassword = await bcrypt.hash(id, salt);
+      let query1=`SELECT * FROM submission where id='${id}' and idcustmer='${idcustmer}'`;
+      pool.query(query1, [idcustmer,id], function(err,results) {
+        if (err) {
+          console.error('Error storing user in database:', err);
+          return res.status(500).json({ error: 'An error occurred while add cv' });
+        }
+        else if(results.length ===0){
+      let query="INSERT INTO submission (idjob,idcustmer,cv,coverletterm,id)VALUES(?,?,? ,?,?,?) ";
+      pool.query(query, [idcustmer,id,hashedPassword], function(err) {
+        if (err) {
+          console.error('Error storing user in database:', err);
+          return res.status(500).json({ error: 'An error occurred while creating the user' });
+        }
+        console.log( "User add successfully");
+        return res.status(201).json({ message: 'User add successfully' });
+      });
+    }
+    else {
+      console.log( "The User already exists");
+      return res.status(201).json({ message: 'The user already exists' });
+    }
+    });
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return res.status(500).json({ error: 'An error occurred while creating the user' });
+    }
+  });
 
-  } catch (e) {
-      res.status(400).send(e)
-  }
+
+//////////
+app.listen(3000, function () {
+  console.log('Express server is listening on port 3000');
 });
-//delete job
-router.delete("/deletejob/:idjob",async (req,res)=>{
-  const _jobid = req.params.idjob
-  const count = await jobs.destroy({ where: { idjob: _jobid } });
-  res.send(`deleted row(s): ${count}`);
-});
-
-// update one job
-router.put('/updatejob/:idjob', async (req, res) => {
-  const _jobid = req.params.idjob
-
-  const Description = req.body.Description
-
-
-  const [updatedRows] = await jobs.update(
-      {
-          Description: Description,
-      },
-      {
-          where: { idjob: _jobid },
-      }
-  );
-
-  if (updatedRows) {
-      res.send(`Updated rows: ${updatedRows}`);
-  } else {
-      res.send("job not found");
-  }
-})
-/////////
-//change password
-router.put('/changePassword/:id', async (req, res) => {
-  const userid = req.params.id
-  const newpass = req.body.new
-  const oldpass = req.body.old
-
-
-  const [updatedRows] = await users.update(
-      {
-          Password: newpass
-      },
-      {
-          where: { id: userid , Password:oldpass },
-      }
-  );
-
-  if (updatedRows) {
-      res.send(`password changed`);
-  } else {
-      res.send("wrong password");
-  }
-})
-//add cv
-router.post("/addcv",async (req,res)=>{
-  const cv = new cvs(req.body)
-  try {
-      await cv.save()
-      res.status(201).send({cv})
-
-  } catch (e) {
-      res.status(400).send(e)
-  }
-});
-//delete job
-router.delete("/deletecv/:id",async (req,res)=>{
-  const _id = req.params.id
-  const count = await cvs.destroy({ where: { id: _jd } });
-  res.send(`deleted row(s): ${count}`);
-});
-
-// update onecv
-router.put('/updatecv/:id', async (req, res) => {
-  const _id = req.params.id
-
-  const Description = req.body.Description
-
-
-  const [updatedRows] = await cvs.update(
-      {
-          Description: Description,
-      },
-      {
-          where: { id: _id },
-      }
-  );
-
-  if (updatedRows) {
-      res.send(`Updated rows: ${updatedRows}`);
-  } else {
-      res.send("job not found");
-  }
-})
