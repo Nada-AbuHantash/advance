a5a5c01b37256beca3bbe68a28429c7be1199dcf
marah <Marahqbj>
2023-05-21 15:36:19 +0300

    marah

--- project.js
+++ project.js
@@ -2,6 +2,7 @@
 const mysql = require("mysql");
 const util = require("util");
 var express = require('express');
+var session = require('express-session');
 var app = express();
 var bodyparser = require('body-parser');
 const { log, count } = require("console");
@@ -10,15 +11,6 @@ const bcrypt = require('bcrypt');
 // const { https } = require('https');
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-
-
-
-const app = express()
-const PORT = process.env.PORT
-const router = express.Router()
-
-
-
 
 const pool = mysql.createConnection({
 
@@ -33,6 +25,11 @@ const pool = mysql.createConnection({
     //   // other SSL/TLS options if needed
     // },
 });
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 console.log("hii");
 pool.connect((err) => {
@@ -40,12 +37,19 @@ pool.connect((err) => {
     console.log('Connected to MySQL database');
   });
   
-//////////marah
+////////////////////////////////////////////////
+app.get('/www.linkedin.com', async (req, res) => {
+   
+  return res.status(400).json({ message: 'Welcome to the LinkedIn application' });
+    
+          });
+
+
 app.post('/www.linkedin.com/register', async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-      return res.status(400).json({ error: 'Name, email, and password are required' });
-    }
+      return res.status(200).json({ error: 'Name, email, and password are required' });
+                              }
   
     try {
        
@@ -59,14 +63,14 @@ app.post('/www.linkedin.com/register', async (req, res) => {
             return res.status(500).json({ error: 'An error occurred while creating the user' });
           }
           else if(results.length ===0){
-        let query="INSERT INTO login (name,email,password)VALUES(?,?,?) ";
-        pool.query(query, [name,email,hashedPassword], function(err) {
-          if (err) {
-            console.error('Error storing user in database:', err);
-            return res.status(500).json({ error: 'An error occurred while creating the user' });
-          }
-          console.log( "User created successfully");
-          return res.status(201).json({ message: 'User created successfully' });
+                      let query="INSERT INTO login (name,email,password)VALUES(?,?,?) ";
+                       pool.query(query, [name,email,hashedPassword], function(err) {
+                             if (err) {
+                                 console.error('Error storing user in database:', err);
+                                 return res.status(500).json({ error: 'An error occurred while creating the user' });
+                                      }
+                       console.log( "User created successfully");
+                       return res.status(201).json({ message: 'User created successfully' });
         });
       }
       else {
@@ -80,6 +84,9 @@ app.post('/www.linkedin.com/register', async (req, res) => {
       }
     });
  
+
+
+
     app.post('/www.linkedin.com/login', async (req, res) => {
       const { email, password } = req.body;
     
@@ -89,79 +96,110 @@ app.post('/www.linkedin.com/register', async (req, res) => {
       }
     
       try {
-        pool.query('SELECT * FROM login WHERE email = ?', email, async (err, results) => {
-          if (err) {
-            console.error('Error in database:', err);
-            return res.status(500).json({ error: 'An error occurred while retrieving user data' });
-          }
+           pool.query('SELECT * FROM login WHERE email = ?', email, async (err, results) => {
+             if (err) {
+              console.error('Error in database:', err);
+              return res.status(500).json({ error: 'An error occurred while retrieving user data' });
+                      }
     
-          if (results.length === 0) {
-            return res.status(404).json({ error: 'User not found' });
-          }
-          const pass = results[0].password;
-          const isPasswordMatch = await bcrypt.compare(password, pass);
-          if (!isPasswordMatch) {
-            return res.status(401).json({ error: 'Invalid password' });
-          }
+             if (results.length === 0) {
+              return res.status(404).json({ error: 'User not found' });
+                                       }
+             const pass = results[0].password;
+             const isPasswordMatch = await bcrypt.compare(password, pass);
+             if (!isPasswordMatch) {
+               return res.status(401).json({ error: 'Invalid password' });
+                                   }
           //console.log.(results);
-          return res.status(200).send(results);
-        });
+                      req.session.useremail =email ;
+                       req.session.pass=password;
+                       return res.status(200).json({ message: 'Login successful' });
+            });
       } catch (error) {
         console.error('Error in database:', error);
         return res.status(500).json({ error: 'An error occurred while retrieving user data' });
       }
     });
+
+    app.get('/www.linkedin.com/logout', (req, res) => {
+      req.session.destroy((err) => {
+        if (err) {
+          console.error('Error destroying session:', err);
+        }
+        res.send('Logged out successfully.');
+      });
+    });
     
-//////////
-  app.listen(3000, function () {
-    console.log('Express server is listening on port 3000');
+    
+
+
+    app.put('/www.linkedin.com/profile', async (req, res) => {
+      const { name, password,email } = req.body;
+      const emailuser = req.session.useremail;
+      if (!email ) {
+        return res.status(400).json({ error: 'Email and password are required' });
+      }
+      try{
+        pool.query('SELECT * FROM login WHERE email = ?', emailuser, async (err, results) => {
+          if (err) {
+           console.error('Error in database:', err);
+           return res.status(500).json({ error: 'An error occurred while retrieving user data' });
+                   }
+                   if (results.length === 0) {
+                    return res.status(404).json({ error: 'User not found' });
+                                             }
+                                             const id=results[0].id;
+                                             const saltRounds = 10;
+                                             const salt = await bcrypt.genSalt(saltRounds);
+                                             const hashedPassword = await bcrypt.hash(password, salt);
+                                             let query='UPDATE login SET name=? , password=? , email=? WHERE id=? ';
+                                             pool.query(query, [name,hashedPassword,email,id], function(err) {
+                                                   if (err) {
+                                                       console.error('Error storing user in database:', err);
+                                                       return res.status(500).json({ error: 'An error occurred while Update profile' });
+                                                            }
+                                                            req.session.useremail =email ;
+                                                             req.session.id=id;
+                                             console.log( "Update profile successfully");
+                                             return res.status(201).json({ message: 'Update profile successfully' });
+                              });                       
+
+
 });
 
 
-//add new job
-//idjob	idcustmer	cv	coverletter	id	
+      }catch (error) {
+        console.error('Error in database:', error);
+        return res.status(500).json({ error: 'An error occurred while retrieving user data' });
+      } 
+    
+                  
+        
+              });
 
-app.post('/www.linkedin.com/addcv', async (req, res) => {
-  const { idjob, idcustmer, cv,coverletterm,id } = req.body;
-  if (!idjob || !idcustmer || !cv  || !coverletterm || !id) {
-    return res.status(400).json({ error: 'are required' });
-  }
 
-  try {
-     
-      const saltRounds = 10;
-      const salt = await bcrypt.genSalt(saltRounds);
-      const hashedPassword = await bcrypt.hash(id, salt);
-      let query1=`SELECT * FROM submission where id='${id}' and idcustmer='${idcustmer}'`;
-      pool.query(query1, [idcustmer,id], function(err,results) {
-        if (err) {
-          console.error('Error storing user in database:', err);
-          return res.status(500).json({ error: 'An error occurred while add cv' });
-        }
-        else if(results.length ===0){
-      let query="INSERT INTO submission (idjob,idcustmer,cv,coverletterm,id)VALUES(?,?,? ,?,?,?) ";
-      pool.query(query, [idcustmer,id,hashedPassword], function(err) {
-        if (err) {
-          console.error('Error storing user in database:', err);
-          return res.status(500).json({ error: 'An error occurred while creating the user' });
-        }
-        console.log( "User add successfully");
-        return res.status(201).json({ message: 'User add successfully' });
-      });
-    }
-    else {
-      console.log( "The User already exists");
-      return res.status(201).json({ message: 'The user already exists' });
-    }
+
+  ////////////////////////////////////////////add job --marah
+  
+  app.post('/www.linkedin.com/addjob', async (req, res) => {
+    const { title, description, requirements,salary,location,companyname,emailcompany } = req.body;
+    if (!title || !description || !requirements ||!salary || !location || !companyname ||!emailcompany) {
+      return res.status(200).json({ error: 'More information needed to added this job !' });
+                              }
+                              let query="INSERT INTO jobs (title, description, requirements,salary,location,companyname,emailcompany)VALUES (?, ?,?,?,?,?,?) ";
+                              pool.query(query, [title, description, requirements,salary,location,companyname,emailcompany], function(err) {
+                                    if (err) {
+                                        console.error('Error storing user in database:', err);
+                                        return res.status(500).json({ error: 'An error occurred while creating the user' });
+                                             }
+                              console.log( "User created successfully");
+                              return res.status(201).json({ message: 'User created successfully' });
+               });
+                              
     });
-    } catch (error) {
-      console.error('Error creating user:', error);
-      return res.status(500).json({ error: 'An error occurred while creating the user' });
-    }
-  });
+    
 
-
-//////////
-app.listen(3000, function () {
-  console.log('Express server is listening on port 3000');
+/////////////////////////////////////////////////////
+  app.listen(3000, function () {
+    console.log('Express server is listening on port 3000');
 });
